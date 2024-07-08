0df34634fed27ca578145f09844c24c062d44a94
NOMAN1802 <mustakimnoman82@gmail.com>
2024-07-08 23:18:54 +0600

    initial setup

---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from "react";
+
+type TContainerProps ={
+    children: ReactNode,
+  };
+const Container  = ({ children } : TContainerProps ) => {
+  return (
+    <div className='max-w-[2520px] mx-auto xl:px-20 md:px-10 sm:px-2 px-4'>
+      {children}
+    </div>
+  )
+}
+
+export default Container;
---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,114 @@
+/* eslint-disable no-unused-vars */
+import { Link, NavLink } from 'react-router-dom';
+import Button from "../Button/Button";
+import { useState } from 'react';
+
+// import { AuthContext } from '../../providers/AuthProvider';
+//
+
+const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+
+
+
+
+
+  return (
+    <nav className='z-50'>
+      <div className='lg:h-10vh flex flex-col lg:flex-row lg:justify-between lg:p-5 px-6 lg:px-20 border-b'>
+        <div className='flex items-center justify-between'>
+          <Link to='/'> <h2 className='text-3xl font-bold text-black'><span className='text-slate-500'>Click</span>Craft</h2></Link>
+          <button
+            className='lg:hidden focus:outline-none'
+            
+          >
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              className={`h-6 w-6 ${isOpen ? 'text-slate-400' : 'text-black'}`}
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke='currentColor'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='2'
+                d='M4 6h16M4 12h16M4 18h16'
+              />
+            </svg>
+          </button>
+        </div>
+        <ul
+          className={`${isOpen ? 'flex flex-col' : 'hidden'
+            } lg:flex lg:gap-8 lg:mr-16 mt-4 lg:mt-0 text-[18px] lg:flex-row`}
+        >
+
+          <li>
+            <NavLink onClick={() => {
+              
+            }}
+              to="/"
+              className={({ isActive }) => (isActive ? "active1" : "default")}
+              title="Home"
+            >
+              Home
+            </NavLink>
+          </li>  
+
+          <li>
+            <NavLink to='/'
+              className={({ isActive }) => (isActive ? "active1" : "default")}
+              title="Products"
+                 
+              >
+              Products
+            </NavLink>
+          </li>
+
+          <li>
+
+            <NavLink to='/'
+              className={({ isActive }) => (isActive ? "active1" : "default")}
+              title="About Us"
+                 
+              >
+              About Us
+            </NavLink>
+          </li>
+
+          <li>
+
+            <NavLink to='/'
+              className={({ isActive }) => (isActive ? "active1" : "default")}
+              title="Contact Us"
+                 
+              >
+              Contact Us
+            </NavLink>
+          </li>
+
+          <li>
+
+            <NavLink to='/'
+              className={({ isActive }) => (isActive ? "active1" : "default")}
+              title="Dashboard"
+                 
+              >
+              Dashboard
+            </NavLink>
+          </li>
+
+
+        </ul>
+
+        <div className='flex items-center'>
+          
+        </div>
+      </div>
+
+    </nav>
+  );
+};
+
+export default NavBar;
